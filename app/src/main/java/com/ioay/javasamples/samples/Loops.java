160af06d2bf185f86c3d7bba77f7697a9da2b9de
ismailoguzhanay <isoguzay@gmail.com>
2019-05-02 23:52:53 +0300

    Loop Statements added.

--- app/src/main/java/com/ioay/javasamples/samples/Loops.java
+++ app/src/main/java/com/ioay/javasamples/samples/Loops.java
@@ -2,10 +2,41 @@ package com.ioay.javasamples.samples;
 
 public class Loops {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
-        for(int i = 0; i<4; i++){
+        for (int i = 0; i < 4; i++) {
             System.out.println("Numbers : " + i);
         }
+
+
+        int a = 0;
+
+        while (a < 4) {
+            System.out.println("Value of a : " + a);
+            a++;
+        }
+
+        for (int b = 7; b <= 10; b++) {
+            System.out.println("Value of b : " + b);
+        }
+
+        int c = 7;
+
+        while (c < 10) {
+            System.out.println("Value of c: " + c);
+            c++;
+        }
+
+        for (int y = 0; y < 9; y += 2) {
+            System.out.println("Value of y : " + y);
+        }
+
+        int e = 0;
+
+        while (e < 9) {
+            System.out.println("Value of e : " + e);
+            e = e + 2;
+        }
+
     }
 }
