a50125a43733a55b32945b9e7772ffc5f75cd567
ioay <oguzhan.ay@betek.com.tr>
2019-05-02 23:32:03 +0300

    Loop (for & while) added.

--- app/src/main/java/com/ioay/javasamples/samples/ForLoop.java
+++
@@ -1,9 +0,0 @@
-package com.ioay.javasamples.samples;
-
-public class ForLoop {
-
-    public static void main(String args[]){
-
-
-    }
-}
---
+++ app/src/main/java/com/ioay/javasamples/samples/Loops.java
@@ -0,0 +1,11 @@
+package com.ioay.javasamples.samples;
+
+public class Loops {
+
+    public static void main(String args[]){
+
+        for(int i = 0; i<4; i++){
+            System.out.println("Numbers : " + i);
+        }
+    }
+}
