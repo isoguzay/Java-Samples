95912a1f2a08d437ba0115a5d78d62c46c096521
ioay <oguzhan.ay@betek.com.tr>
2019-05-05 16:09:59 +0300

    ArrayOperations added.

---
+++ app/src/main/java/com/ioay/javasamples/samples/ArrayOperations.java
@@ -0,0 +1,24 @@
+package com.ioay.javasamples.samples;
+
+import java.util.Arrays;
+
+public class ArrayOperations {
+
+    public static void main(String args[]) {
+
+        int luckyNumbers[] = new int[6];
+
+        luckyNumbers[0] = 34;
+        luckyNumbers[1] = 35;
+        luckyNumbers[3] = 75;
+
+        System.out.println(Arrays.toString(luckyNumbers));
+
+        String luckyDays[] = new String[3];
+
+        luckyDays[0] = "Saturday";
+
+        System.out.println(luckyDays);
+
+    }
+}
