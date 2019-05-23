15b056880e523d5506543cf22ff44ad4140b7588
ioay <oguzhan.ay@betek.com.tr>
2019-05-23 09:14:05 +0300

    String Reverse added.

--- app/src/main/java/com/ioay/javasamples/examples/stringsample/main/StringReverseMain.java
+++ app/src/main/java/com/ioay/javasamples/examples/stringsample/main/StringReverseMain.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class StringReverseMain {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         Scanner scanner = new Scanner(System.in);
 
@@ -14,13 +14,13 @@ public class StringReverseMain {
 
         int length = word.length();
 
-        char [] array = new char[length];
+        char[] array = new char[length];
 
-        for (int i = 0; i<length; i++){
+        for (int i = 0; i < length; i++) {
             array[i] = word.charAt(i);
         }
 
-        for (int i = (length-1); i>=0; i--){
+        for (int i = (length - 1); i >= 0; i--) {
             System.out.print(array[i]);
         }
     }
